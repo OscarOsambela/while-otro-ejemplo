85a2437ef161d2f932993050d3d8d39c39edb5b9
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-11-13 09:55:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System; 
+
+class MainClass {
+  public static void Main (string[] args) {
+    int numero = 0;
+    while(numero <= 10){
+      Console.WriteLine(numero);
+      numero = numero + 2;
+    }
+  }
+}
